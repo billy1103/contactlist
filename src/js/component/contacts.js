d0f58652c801f346bd170edfd1ec0ca4af7c3d22
billy1103 <billy.0311@hotmail.com>
2022-04-14 00:51:49 +0000

    first

---
+++ src/js/component/contacts.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const People = () => {
+    <ul>
+        <li>
+        <Link>
+					hello
+				</Link>
+        </li>
+    </ul>
+} 
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,15 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	return (
+		<nav className="mb-3">
+			<div className="ml-auto">
+				<Link to="/demo">
+					<button className="btn btn-success">Add new contact</button>
+				</Link>
+			</div>
+		</nav>
+	);
+}
